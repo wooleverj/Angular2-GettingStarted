84e5083209f2c8cbd4d808cb52c52275e1e62b2b
Woolever <jeremy.woolever@corp.Altegrity.com>
2016-12-16 14:29:26 -0600

    added component

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,11 +1,11 @@
+import { ProductListComponent } from './products/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProductListComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
